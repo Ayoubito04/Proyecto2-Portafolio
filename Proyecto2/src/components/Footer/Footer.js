b1c4bcf1cc08c7c791580efd68470f196d3d9e19
unknown <ayoubarramdani091@gmail.com>
2025-04-25 15:17:52 +0200

    Ajuste y correción en la ruta de imagenes

--- Proyecto2/src/components/Footer/Footer.js
+++ Proyecto2/src/components/Footer/Footer.js
@@ -1,17 +1,18 @@
 import "./Footer.css";
 import { Button } from "../Button/Button";
+import Github from "../../assets/img/Github.png";
+import Linkedin from "../../assets/img/Linkedin.png";
+import Instagram from "../../assets/img/Instagram.png";
 
 
 export const Footer=()=>{
     return `
    <h2 class="footer-title">Puede ver mi perfil en:</h2>
     <div class="footer-container">
-    ${Button("./Github.png","GitHub")}
-    ${Button("./Linkedin.png","LinkedIn")}
-    ${Button("./Instagram.png","Instagram")}
-
-
-    
+        ${Button(Github, "Github")}
+        ${Button(Linkedin, "Linkedin")}
+        ${Button(Instagram, "Instagram")}
+       
     </div>
 `
 }
